b5bc616d792d583f5c9c07a0024ba0a320484f80
Nicolas Ramos <ramos.nicolas@hotmail.es>
2021-07-13 13:23:30 +0200

    First commit

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+
+import "./ExpenseForm.css";
+
+const ExpenseForm = (props) => {
+    const [enteredTitle, setEnteredTitle] = useState("");
+    const [enteredAmount, setEnteredAmount] = useState("");
+    const [enteredDate, setEnteredDate] = useState("");
+    // const [userInput, setUserInput] = useState({
+    //     enteredTitle: "",
+    //     enteredAmount: "",
+    //     enteredDate: "",
+    // });
+
+    const titleChangeHandler = (event) => {
+        setEnteredTitle(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredTitle: event.target.value,
+        // });
+    };
+
+    const amountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredAmount: event.target.value,
+        // });
+        // setUserInput((prevState) => {
+        //     return { ...prevState, enteredAmount: event.target.value };
+        // });
+    };
+
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredAmount: event.target.value,
+        // });
+    };
+
+    const submitHandler = (event) => {
+        event.preventDefault(); //? Evitar que mande el request y refresque la página
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: +enteredAmount, //El + es para forzarlo a hacer una conversion a número
+            date: new Date(enteredDate),
+        };
+
+        /* if (props.onSaveExpenseData) */ props.onSaveExpenseData(expenseData);
+
+        //? Reiniciar los inputs
+        setEnteredTitle("");
+        setEnteredAmount("");
+        setEnteredDate("");
+    };
+
+    return (
+        <form onSubmit={submitHandler}>
+            <div className="new-expense__controls">
+                <div className="new-expense__control">
+                    <label>Título</label>
+                    <input
+                        type="text"
+                        value={enteredTitle}
+                        onChange={titleChangeHandler}
+                    />
+                </div>
+                <div className="new-expense__control">
+                    <label>Monto</label>
+                    <input
+                        type="number"
+                        min="0.01"
+                        step="0.01"
+                        value={enteredAmount}
+                        onChange={amountChangeHandler}
+                    />
+                </div>
+                <div className="new-expense__control">
+                    <label>Fecha</label>
+                    <input
+                        type="date"
+                        min="2019-01-01"
+                        max="2022-12-31"
+                        value={enteredDate}
+                        onChange={dateChangeHandler}
+                    />
+                </div>
+            </div>
+            <div className="new-expense__actions">
+                <button type="button" onClick={props.onCancel}>
+                    Cancelar
+                </button>
+                <button type="submit">Agregar Gasto</button>
+            </div>
+        </form>
+    );
+};
+
+export default ExpenseForm;
